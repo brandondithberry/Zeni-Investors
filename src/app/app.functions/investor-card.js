f6431b5127bdf3274977e923a677a9c9550496c1
Brandon Dith-Berry <31754210+brandondithberry@users.noreply.github.com>
2023-06-07 15:27:50 +0000

    Child Company

--- src/app/app.functions/investor-card.js
+++ src/app/app.functions/investor-card.js
@@ -9,7 +9,7 @@ exports.main = async (context = {}, sendResponse) => {
       {
           type: 'crm::table',
           objectTypeId: '0-2',
-          properties: ['name', 'domain', 'hs_object_id'],
+          properties: ['name', 'company_ceo', 'industry', 'city', 'state', 'hs_analytics_latest_source', 'hs_analytics_latest_source_data_1', 'hs_analytics_latest_source_data_2', 'hs_analytics_latest_source_timestamp', 'total_number_of_touchpoints'],
           pageSize: 10
       }
       ],
