eeb77d43f2e2a93dd148b545c2e5af5f55d3e189
Brandon Dith-Berry <31754210+brandondithberry@users.noreply.github.com>
2023-06-07 15:14:57 +0000

    Company Table

--- src/app/app.functions/investor-card.js
+++ src/app/app.functions/investor-card.js
@@ -1,8 +1,17 @@
-[
-  {
-    type: "crm::table",
-    objectTypeId: "companies",
-    properties: ["name", "domain"],
-    pageSize: 10,
-  },
-];
+exports.main = async (context = {}, sendResponse) => {
+    sendResponse({
+      sections:  [
+        {
+          "type":"text",
+          "format":"markdown",
+          "text": "A table that displays company data."
+      },
+      {
+          type: 'crm::table',
+          objectTypeId: '0-2',
+          properties: ['name', 'domain'],
+          pageSize: 3
+      }
+      ],
+    });
+};
