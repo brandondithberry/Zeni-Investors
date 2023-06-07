ac214b4148eebfcd949026f3a9f0c530564ecf70
Brandon Dith-Berry <31754210+brandondithberry@users.noreply.github.com>
2023-06-07 15:34:49 +0000

    CEO

--- src/app/app.functions/investor-card.js
+++ src/app/app.functions/investor-card.js
@@ -9,7 +9,7 @@ exports.main = async (context = {}, sendResponse) => {
       {
           type: 'crm::table',
           objectTypeId: '0-2',
-          properties: ['name', 'company_ceo', 'industry', 'city', 'state', 'hs_analytics_latest_source', 'hs_analytics_latest_source_data_1', 'hs_analytics_latest_source_data_2', 'hs_analytics_latest_source_timestamp', 'total_number_of_touchpoints'],
+          properties: ['name', 'ceo', 'industry', 'city', 'state', 'hs_analytics_latest_source', 'hs_analytics_latest_source_data_1', 'hs_analytics_latest_source_data_2', 'hs_analytics_latest_source_timestamp', 'total_number_of_touchpoints'],
           pageSize: 10
       }
       ],
